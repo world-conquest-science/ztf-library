cd4c0e112023f71fc85c681baec7bcbe61b81c3d
Jean-Jacques Akakpo <akakpo.jeanjacques@gmail.com>
2024-11-12 23:04:59 +0100

    🐛 Update due to next-intl updates

--- apps/frontend/app/i18n/routing.ts
+++ apps/frontend/app/i18n/routing.ts
@@ -1,11 +1,11 @@
 import { defineRouting } from 'next-intl/routing'
-import { createSharedPathnamesNavigation } from 'next-intl/navigation'
+import { createNavigation } from 'next-intl/navigation'
 
 export const routing = defineRouting({
   locales: ['en', 'fr'],
   defaultLocale: 'en',
 })
 
-export const { Link, redirect, usePathname, useRouter } = createSharedPathnamesNavigation(routing)
+export const { Link, redirect, usePathname, useRouter } = createNavigation(routing)
 
 export type TLocale = (typeof routing.locales)[number]
