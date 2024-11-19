2880fb8baadeb8e6a2b9730b5d57d9c2806ee80a
Jean-Jacques Akakpo <akakpo.jeanjacques@gmail.com>
2024-11-19 13:22:18 +0100

    🎨 Reformat Books component

--- apps/frontend/app/[locale]/page.tsx
+++ apps/frontend/app/[locale]/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Banner } from '@/app/components/Banner'
 import { SpecialOffers } from '@/app/components/SpecialOffers'
-import { BooksFromCategory } from '@/app/components/BooksFromCategory'
+import { Books } from '@/app/components/Books'
 import { QuotesFromAuthor } from '@/app/components/QuotesFromAuthor'
 import { PopularSeries } from '@/app/components/PopularSeries'
 import { InfiniteBrowser } from '@/app/components/InfiniteBrowser'
@@ -11,8 +11,7 @@ const HomePage = () => {
     <>
       <Banner />
       <SpecialOffers />
-      <BooksFromCategory />
-      <BooksFromCategory />
+      <Books />
       <QuotesFromAuthor />
       <PopularSeries />
       <InfiniteBrowser />
--- apps/frontend/app/components/Books/index.tsx
+++ apps/frontend/app/components/Books/index.tsx
@@ -1,13 +1,22 @@
+import { useTranslations } from 'next-intl'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 const BookItem = () => {
   return (
-    <li className="overflow-hidden rounded-none">
-      <Link href={'url'} className="">
-        <div className="">
-          <Image src="/images/books/destinies.png" alt="Book name" width={300} height={0} />
+    <li className="w-[40vw] min-w-[200px] p-3 pl-0 sm:w-auto sm:p-0">
+      <Link
+        href={'url'}
+        className="flex w-full flex-col overflow-hidden rounded-none sm:flex-row sm:justify-items-start"
+      >
+        <div className="w-full sm:w-auto sm:flex-1">
+          <Image
+            src="/images/books/destinies.png"
+            alt="Book name"
+            width={300}
+            height={0}
+          />
         </div>
       </Link>
     </li>
@@ -15,19 +24,33 @@ const BookItem = () => {
 }
 
 const Books = () => {
-  const books = new Array(76).fill({ url: '' }).map((item, index) => ({
-    ...item,
-    url: index.toString(),
-  }))
+  const gTrans = useTranslations('Global')
 
   return (
-    <section className="px-6 sm:px-0 sm:pt-28">
+    <section className="mt-10 bg-white px-6 sm:mt-20 sm:px-0">
       <div className="container mx-auto">
-        <ul className="grid grid-cols-2 gap-5 sm:grid-cols-6 sm:gap-10">
-          {books.map(book => (
-            <BookItem key={book.url} />
-          ))}
-        </ul>
+        <header className="flex flex-col items-start sm:mb-3 sm:flex-row sm:items-center sm:justify-between">
+          <h4 className="text-2xl font-extrabold text-accent-dark sm:text-4xl">
+            The christian way
+          </h4>
+          <button className="text-base font-bold text-primary-600 underline underline-offset-4 sm:text-lg">
+            {gTrans('show-all')}
+          </button>
+        </header>
+        <div className="overflow-x-scroll">
+          <ol className="my-5 flex w-fit flex-nowrap justify-center sm:gap-10">
+            <BookItem />
+            <BookItem />
+            <BookItem />
+            <BookItem />
+            <BookItem />
+            <BookItem />
+            <BookItem />
+            <BookItem />
+            <BookItem />
+            <BookItem />
+          </ol>
+        </div>
       </div>
     </section>
   )
